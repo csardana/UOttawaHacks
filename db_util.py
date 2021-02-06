106c2fe73be8ffbc3459077e669cb846d238916c
Mark Kaiser <markkaiser@uvic.ca>
2021-02-05 22:38:09 -0800

    db functions and test py script for db

---
+++ db_util.py
@@ -0,0 +1,112 @@
+from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from cassandra.policies import WhiteListRoundRobinPolicy, DowngradingConsistencyRetryPolicy
+from cassandra.query import tuple_factory
+from cassandra.auth import PlainTextAuthProvider
+import uuid
+
+def createSession():
+    cloud_config= {
+        'secure_connect_bundle': './secure-connect-uottahack.zip'
+    }
+    auth_provider = PlainTextAuthProvider('uottahack', 'c975SKEuyvXW!pU')
+    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    session = cluster.connect('uottahack')
+    return session
+
+def retrieveLeaderboard(session):
+    leaderboard = session.execute(
+        """
+        SELECT * FROM Leaderboard;
+        """
+    )
+    return leaderboard
+
+def executePreparedStatement(session, prepSt, params):
+    if type(params) is not list:
+        params = list(params)
+    try:
+        st = session.prepare(prepSt)
+        data = session.execute(st, params)
+    except Exception as e:
+        print(e)
+        return 1
+
+    return 0
+
+def insertNickname(session, login_info):
+    user_id = uuid.uuid1()
+    try:
+        st = session.prepare(
+            """
+            INSERT INTO Nicknames (user_id, dob, firstname, lastname, nickname) VALUES (?, ?, ?, ?, ?)
+            """
+        )
+        status = session.execute(st, (
+                user_id, 
+                login_info['dob'], 
+                login_info['firstname'], 
+                login_info['lastname'], 
+                login_info['nickname']
+            )
+        )
+        st2 = session.prepare(
+            """
+            INSERT INTO Leaderboard (user_id, guessscore, hintscore, totalscore) VALUES (?, ?, ?, ?)
+            """
+        )
+        status2 = session.execute(st2, (user_id, 0, 0, 0))
+    except Exception as e:
+        print(e)
+        return 1
+    
+    return 0
+
+def updateScore(session, addedScore, user_id, scoreType):
+    try:
+        st = session.prepare(
+            "SELECT " + scoreType + ", totalScore FROM Leaderboard WHERE user_id = ?"
+        )
+        scores = int(session.execute(st, (user_id)))
+        score0 = scores[0] + addedScore
+        totScore = scores[1] + addedScore
+    except Exception as e:
+        print(e)
+        return 1
+
+    try:
+        statement = "UPDATE Leaderboard SET " + scoreType + " = ?, totalScore = ? WHERE user_id = ?"
+        st = session.prepare(statement)
+        status = session.execute(st, (score0, totScore, user_id))
+    except Exception as e:
+        print(e)
+        return 1
+
+    return 0
+
+def resetScores(session):
+    try:
+        status = session.execute(
+            """
+            UPDATE Leaderboard SET guessScore = 0, hintScore = 0, totalScore = 0
+            """
+        )
+    except Exception as e:
+        print(e)
+        return 1
+    
+    return 0
+
+def getUseridFromNickname(session, nickname):
+    #should be mainly for testing
+    try:
+        st = session.prepare(
+            """
+            SELECT user_id FROM Nicknames WHERE nickname = ?
+            """
+        )
+        user_id = session.execute(st, (nickname))
+    except Exception as e:
+        print(e)
+        return 1
+    
+    return user_id
---
+++ test_db.py
@@ -0,0 +1,24 @@
+import db_util as db
+import uuid
+
+session = db.createSession()
+
+print(session)
+
+login_info = {
+    'dob': '1999-03-02',
+    'firstname': 'Mark',
+    'lastname': 'Test',
+    'nickname': 'RapidBadger'
+}
+
+#s1 = db.insertNickname(session, login_info)
+print("s2")
+s2 = db.retrieveLeaderboard(session)
+for row in s2:
+    print (row[0], row[1], row[2], row[3])
+print("s3")
+s3 = db.getUseridFromNickname(session, login_info['nickname'])
+print(s3)
+print("s4")
+s4 = db.updateScore(session, 40, s3, "guessscore")
