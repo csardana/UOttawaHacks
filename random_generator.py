18d07efa43e37977650745eec95f3068af867753
Chirag Sardana <csardana18@gmail.com>
2021-02-06 02:08:22 -0500

    Random name generator
    Random name generator , generating a name in userName variable . Run and tested. Using validate function to check if first , last or DOB not included .

---
+++ random_generator.py
@@ -0,0 +1,46 @@
+import requests
+from random import randint
+
+firstName = 'Chirag'
+lastName = 'Sardana'
+
+DOB = '02-04-1998'
+
+userName = ''
+
+[month, year, day] = DOB.split('-')
+
+
+def generate():
+    global userName
+    URL = 'https://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text%2Fplain'
+    names = requests.get(URL,headers={'User-Agent': 'Custom'})
+
+    namesText = names.text
+    namesSeperated = namesText.split()
+
+
+    randomInt = randint(0, len(namesSeperated))
+    fuser = namesSeperated[randomInt]
+    num = str(randomInt)
+    userName = fuser + num[:2]
+
+
+def validate():
+    global firstName
+    global lastName
+    global month
+    global year
+    global day
+
+    if firstName in userName:
+        generate()
+    if lastName in userName:
+        generate()
+    if month or day or year in userName:
+        generate()
+
+
+generate()
+validate()
+//print(userName)
