2d802e2c6f26723b6ad302264dd3ee2e49f42b19
Chirag Sardana <csardana18@gmail.com>
2021-02-07 02:58:47 -0500

    did some changes
    did some changes

--- login/script.js
+++ login/script.js
@@ -7,16 +7,27 @@ $('.toggle').on('click', function() {
   });
 
 function get_inputs(){
-    var first_name = document.getElementById('#{label}')[0].value
-    var Last_name = document.getElementById('#{label}')[1].value
-    var DOB = document.getElementById('#{label}')[2]
+    var first_name = document.getElementById('#{label}')[0];
+    var Last_name = document.getElementById('#{label}')[1];
+    var DOB = document.getElementById('#{label}')[2];
+    var entry = {
+    firstname : first_name.value
+    lastname : Last_name.value
+    dob : DOB.value
+    };
 
-
-      fetch(`/createNickname`)
+      fetch(`${window.origin}/createNickname`,{
+      method : "POST"
+      credentials : "include"
+      body : JSON.stringify(entry),
+      cache : "no-cache",
+      headers : new Headers({
+      "content-type" : "application/json"})})
           .then(function (response) {
               return response.text();
           }).then(function (text) {
               console.log('GET response text:');
               console.log(text);
           });
+
 }
