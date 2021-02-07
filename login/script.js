03bb1033d635d2ec489849c53ed430dd3bb04354
Chirag Sardana <csardana18@gmail.com>
2021-02-07 02:37:09 -0500

    Changed with flask
    Changed with flask

--- login/script.js
+++ login/script.js
@@ -5,3 +5,18 @@ $('.toggle').on('click', function() {
   $('.close').on('click', function() {
     $('.container').stop().removeClass('active');
   });
+
+function get_inputs(){
+    var first_name = document.getElementById('#{label}')[0].value
+    var Last_name = document.getElementById('#{label}')[1].value
+    var DOB = document.getElementById('#{label}')[2]
+
+
+      fetch(`/createNickname`)
+          .then(function (response) {
+              return response.text();
+          }).then(function (text) {
+              console.log('GET response text:');
+              console.log(text);
+          });
+}
